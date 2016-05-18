0f8cf6ddda3d2d6f25dc77de5ffa8b80ee56c001
pavanbanakar <pavancbz1@gmail.com>
2016-05-17 22:56:22 -0400

    "Functions, Rest variables"

--- HelloTypeScriptVS/HelloTypeScriptVS/app.js
+++ HelloTypeScriptVS/HelloTypeScriptVS/app.js
@@ -1,10 +1,11 @@
 function GetAllBooks() {
-    var books = [{ title: "Shanataram", author: "Greogory", available: true, category: Category.Fiction }, {
-            title: "An Astronounts Guide to Life on Earth", author: "Chris HadField", available: true, category: Category.Biography
-        }, { title: "Dark Places", author: "Gillian Flynn", available: true, category: Category.Fiction }];
+    var books = [{ id: 1, title: "Shanataram", author: "Greogory", available: true, category: Category.Fiction }, {
+            id: 2, title: "An Astronounts Guide to Life on Earth", author: "Chris HadField", available: true, category: Category.Biography
+        }, { id: 3, title: "Dark Places", author: "Gillian Flynn", available: true, category: Category.Fiction }];
     return books;
 }
 function LogFirstAvailable(books) {
+    if (books === void 0) { books = GetAllBooks(); }
     var numberOfBooks = books.length;
     var firstAvailableBook = '';
     for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
@@ -26,6 +27,7 @@ var Category;
 })(Category || (Category = {}));
 ;
 function GetBookTitleByCategory(categoryFilter) {
+    if (categoryFilter === void 0) { categoryFilter = Category.Fiction; }
     console.log('Getting Books for the category: ' + Category[categoryFilter]);
     var allBooks = GetAllBooks();
     var filteredTitles = [];
@@ -43,6 +45,39 @@ function LogBookTitles(titles) {
         console.log(title);
     }
 }
-var titles = GetBookTitleByCategory(Category.Biography);
-LogBookTitles(titles);
+function GetBookByID(id) {
+    var books = GetAllBooks();
+    return books.filter(function (book) { return book.id === id; })[0];
+}
+function CreateCustomer(name, age, city) {
+    console.log('Customer Name: ' + name);
+    if (age)
+        console.log('Customer Age: ' + age);
+    if (city)
+        console.log('Customer City: ' + city);
+}
+function CheckOutBooks(customer) {
+    var bookIDs = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        bookIDs[_i - 1] = arguments[_i];
+    }
+    var booksCheckedOut = [];
+    for (var _a = 0, bookIDs_1 = bookIDs; _a < bookIDs_1.length; _a++) {
+        var id = bookIDs_1[_a];
+        var book = GetBookByID(id);
+        if (book.available) {
+            booksCheckedOut.push(book.title);
+        }
+        ;
+    }
+    return booksCheckedOut;
+}
+//*****************************************
+var titles = CheckOutBooks('Chet', 1, 3);
+titles.forEach(function (title) { return console.log('Checked out Book: ' + title); });
+//CreateCustomer('Danielle', 28, 'Vaughan');
+//const titles = GetBookTitleByCategory(Category.Fiction);
+//titles.forEach((val, index, arr) => console.log(val));
+//console.log(GetBookByID(1).author);
+//LogFirstAvailable();
 //# sourceMappingURL=app.js.map
--- HelloTypeScriptVS/HelloTypeScriptVS/app.ts
+++ HelloTypeScriptVS/HelloTypeScriptVS/app.ts
@@ -1,12 +1,12 @@
 ﻿function GetAllBooks() {
-    let books = [{ title: "Shanataram", author: "Greogory", available: true, category: Category.Fiction }, {
-        title: "An Astronounts Guide to Life on Earth", author: "Chris HadField", available: true, category: Category.Biography
-    }, { title: "Dark Places", author: "Gillian Flynn", available: true, category: Category.Fiction }];
+    let books = [{ id: 1, title: "Shanataram", author: "Greogory", available: true, category: Category.Fiction }, {
+        id: 2, title: "An Astronounts Guide to Life on Earth", author: "Chris HadField", available: true, category: Category.Biography
+    }, { id: 3, title: "Dark Places", author: "Gillian Flynn", available: true, category: Category.Fiction }];
 
     return books;
 }
 
-function LogFirstAvailable(books): void {
+function LogFirstAvailable(books = GetAllBooks()): void {
     let numberOfBooks: number = books.length;
     let firstAvailableBook: string = '';
     for (let currentBook of books) {
@@ -22,7 +22,7 @@ function LogFirstAvailable(books): void {
 
 enum Category { Biography, Fiction, Children, Poetry };
 
-function GetBookTitleByCategory(categoryFilter: Category): Array<string> {
+function GetBookTitleByCategory(categoryFilter: Category =  Category.Fiction): Array<string> {
     console.log('Getting Books for the category: ' + Category[categoryFilter]);
     let allBooks = GetAllBooks();
     const filteredTitles: string[] = [];
@@ -34,9 +34,59 @@ function GetBookTitleByCategory(categoryFilter: Category): Array<string> {
     return filteredTitles;
 }
 
-function LogBookTitles(titles: string[]):void {
+function LogBookTitles(titles: string[]): void {
     for (let title of titles)
         console.log(title);
 }
-const titles = GetBookTitleByCategory(Category.Biography);
-LogBookTitles(titles);
+
+function GetBookByID(id: number) {
+    let books = GetAllBooks();
+    return books.filter(book => book.id === id)[0];
+}
+
+function CreateCustomer(name: string, age?: number, city?: string): void {
+    console.log('Customer Name: ' + name);
+    if (age)
+        console.log('Customer Age: ' + age);
+
+    if (city)
+        console.log('Customer City: ' + city);
+}
+
+function CheckOutBooks(customer: string, ...bookIDs: number[]): string[] {
+
+    let booksCheckedOut: string[] = [];
+
+    for (let id of bookIDs) {
+        let book = GetBookByID(id);
+        if (book.available) {
+            booksCheckedOut.push(book.title);
+        };
+
+    }
+    return booksCheckedOut;
+}
+
+//*****************************************
+
+var titles = CheckOutBooks('Chet', 1,3);
+
+titles.forEach(title => console.log('Checked out Book: '+ title));
+
+//CreateCustomer('Danielle', 28, 'Vaughan');
+
+//const titles = GetBookTitleByCategory(Category.Fiction);
+//titles.forEach((val, index, arr) => console.log(val));
+
+//console.log(GetBookByID(1).author);
+
+//LogFirstAvailable();
+
+
+
+
+
+
+
+
+
